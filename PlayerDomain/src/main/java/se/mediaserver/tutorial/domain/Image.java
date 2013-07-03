aee9922a88c6376d9f2c6ad47db5e5e527b1c287
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-07-03 13:51:31 +0200

    new Image

--- PlayerDomain/src/main/java/se/mediaserver/tutorial/domain/Image.java
+++ PlayerDomain/src/main/java/se/mediaserver/tutorial/domain/Image.java
@@ -27,65 +27,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 @NamedQueries({
     @NamedQuery(name = "Image.findAll", query = "SELECT i FROM Image i"),
     @NamedQuery(name = "Image.findByUuid", query = "SELECT i FROM Image i WHERE i.uuid = :uuid")})
-public class Image implements Serializable {
+public class Image extends Media implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "UUID")
-    private Long uuid;
-
-    @JoinColumn(name = "UUID", referencedColumnName = "UUID", insertable = false, updatable = false)
-    @OneToOne(optional = false)
-    private Media media;
-
     public Image() {
-    }
-
-    public Image(Long uuid) {
-        this.uuid = uuid;
-    }
-
-    public Long getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(Long uuid) {
-        this.uuid = uuid;
-    }
-
-    public Media getMedia() {
-        return media;
-    }
-
-    public void setMedia(Media media) {
-        this.media = media;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (uuid != null ? uuid.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Image)) {
-            return false;
-        }
-        Image other = (Image) object;
-        if ((this.uuid == null && other.uuid != null) || (this.uuid != null && !this.uuid.equals(other.uuid))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "se.mediaserver.tutorial.domain.Image[ uuid=" + uuid + " ]";
     }
 }
