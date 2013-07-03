50d314785744dee82a6addf5def719fd3f4665ee
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-07-03 14:05:09 +0200

    adjusting Media

--- PlayerDomain/src/main/java/se/mediaserver/tutorial/domain/Media.java
+++ PlayerDomain/src/main/java/se/mediaserver/tutorial/domain/Media.java
@@ -12,6 +12,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
@@ -25,6 +27,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @Entity
 @Table(name = "MEDIA")
+@Inheritance(strategy = InheritanceType.JOINED)
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Media.findAll", query = "SELECT m FROM Media m"),
@@ -36,74 +39,32 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "Media.findByVisibility", query = "SELECT m FROM Media m WHERE m.visibility = :visibility")})
 public class Media implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
     @Column(name = "UUID")
     private Long uuid;
 
-    @Size(max = 31)
-    @Column(name = "DTYPE")
-    private String dtype;
-
-    @Size(max = 255)
-    @Column(name = "FILENAME")
-    private String filename;
-
-    @Size(max = 50)
-    @Column(name = "MIME_TYE")
-    private String mimeTye;
-
-    @Size(max = 255)
-    @Column(name = "OWNER")
+    @Column(name = "OWNER", length = 255, table = "MEDIA")
     private String owner;
 
-    @Size(max = 50)
-    @Column(name = "VISIBILITY")
+    @Column(name = "VISIBILITY", length = 50, table = "MEDIA")
     private String visibility;
 
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "media")
-    private Image image;
+    @Column(name = "FILENAME", length = 255, table = "MEDIA")
+    private String filename;
 
-    public Media() {
-    }
+    @Column(name = "MIME_TYE", length = 50, table = "MEDIA")
+    private String mimetype; // anv. Enum
 
-    public Media(Long uuid) {
-        this.uuid = uuid;
-    }
-
+//    @Embedded
+//    private MediaText mediaText;
+    
     public Long getUuid() {
         return uuid;
     }
 
     public void setUuid(Long uuid) {
         this.uuid = uuid;
-    }
-
-    public String getDtype() {
-        return dtype;
-    }
-
-    public void setDtype(String dtype) {
-        this.dtype = dtype;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public String getMimeTye() {
-        return mimeTye;
-    }
-
-    public void setMimeTye(String mimeTye) {
-        this.mimeTye = mimeTye;
     }
 
     public String getOwner() {
@@ -122,36 +83,24 @@ public class Media implements Serializable {
         this.visibility = visibility;
     }
 
-    public Image getImage() {
-        return image;
+    public String getFilename() {
+        return filename;
     }
 
-    public void setImage(Image image) {
-        this.image = image;
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (uuid != null ? uuid.hashCode() : 0);
-        return hash;
+    public String getMimetype() {
+        return mimetype;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Media)) {
-            return false;
-        }
-        Media other = (Media) object;
-        if ((this.uuid == null && other.uuid != null) || (this.uuid != null && !this.uuid.equals(other.uuid))) {
-            return false;
-        }
-        return true;
+    public void setMimetype(String mimetype) {
+        this.mimetype = mimetype;
     }
 
     @Override
     public String toString() {
-        return "se.mediaserver.tutorial.mediaweb.imagewebproject.Media[ uuid=" + uuid + " ]";
+        return "Media-class";
     }
 }
